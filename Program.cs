d436b71672a3cfbd058d0cf3caeb04bc710a939f
Admin <153949503+UtahSTEMandArtsAcademy@users.noreply.github.com>
2024-09-26 19:02:20 -0600

    more work on day 7
    wow i cant belive we still arent done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿string input = @"";
 Dictionary<string, ushort> wires = new();
-
-Console.WriteLine((ushort)(123 | 456));
+List<string> flagged = new();
+//while(){}
 static bool[] ConvertBA (ushort signal)
 {
     bool[] result = new bool[16];
@@ -35,50 +35,77 @@ static bool[] Not(bool[] ba)
     }
     return ba;
 }
-for(int y = 0; y < 26; y++)
+static bool[] RShift(bool[] ba, int count)
 {
-    for(int x = -1; x < 26; x++)
+    if(count == 0) return ba;
+    bool last = ba[15];
+    for(int i = 15; i >= 0; i--) ba[i] = ba[i-1];
+    ba[0] = last;
+
+    return RShift(ba, count - 1);
+}
+static bool[] LShift(bool[] ba, int count)
+{
+    if(count == 0) return ba;
+    bool first = ba[0];
+    for(int i = 0; i >= 15; i++) ba[i] = ba[i+1];
+    ba[15] = first;
+
+    return LShift(ba, count - 1);
+}
+
+void DoCmd()
+{
+    
+    foreach(string cmd in input.Split("\n"))
     {
-        string pair = "";
-        char firstLetter = Convert.ToChar(97+x);
-        if(x != -1) pair += firstLetter;
-        char secondLetter = Convert.ToChar(97+y);
-        pair += secondLetter;
-        wires[pair] = 0;
+        string[] parts = cmd.Split(" ");
+
+        if(parts.Length == 3)
+        {
+            string val = parts[0];
+            string to = parts[2];
+            wires[to] = ushort.Parse(val);
+        }
+        else if(parts.Length == 4)
+        {
+            string val = parts[1];
+            string to = parts[3];
+            wires[to] = ConvertUShort(Not(ConvertBA(ushort.Parse(val))));
+        }
+        else
+        {
+            string wireIn1 = parts[0];
+            string wireIn2 = parts[2];
+            if(!wires.ContainsKey(wireIn1))
+            {
+                flagged.Add(wireIn1); 
+                continue;
+            }
+            if(!wires.ContainsKey(wireIn2))
+            {
+                flagged.Add(wireIn2); 
+                continue;
+            }
+            string command = parts[1];
+            string wireOut = parts[4];
+            if(command == "RSHIFT") wires[wireOut] = ConvertUShort(RShift(ConvertBA(wires[wireIn1]), int.Parse(wireIn2)));
+            if(command == "LSHIFT") wires[wireOut] = ConvertUShort(LShift(ConvertBA(wires[wireIn1]), int.Parse(wireIn2)));
+            if(command == "OR") wires[wireOut] = (ushort)(int.Parse(wireIn1) | int.Parse(wireIn2));
+            if(command == "AND") wires[wireOut] = (ushort)(int.Parse(wireIn1) & int.Parse(wireIn2));
+        }
+    
     }
 }
-foreach(string cmd in input.Split("\n"))
-{
-    string[] parts = cmd.Split(" ");
-    if(parts.Length == 3)
-    {
-        string val = parts[0];
-        string to = parts[2];
-        wires[to] = ushort.Parse(val);
-    }
-    else if(parts.Length == 4)
-    {
-        string val = parts[1];
-        string to = parts[3];
-      wires[to] = ConvertUShort(Not(ConvertBA(ushort.Parse(val))));
-    }
-    else if(parts.Contains("O"))
-    {
-        string val1 = parts[0];
-        string val2 = parts[2];
-        string to = parts[4];
-        wires[to] = (ushort)(int.Parse(val1) | int.Parse(val2));
-    }
-    else if(parts.Contains("RS"))
-    {
-
-    }
-    else if(parts.Contains("L"))
-    {
-
-    }
-    else if(parts.Contains("A"))
-    {
-
-    }
-}
+// for(int y = 0; y < 26; y++)
+// {
+//     for(int x = -1; x < 26; x++)
+//     {
+//         string pair = "";
+//         char firstLetter = Convert.ToChar(97+x);
+//         if(x != -1) pair += firstLetter;
+//         char secondLetter = Convert.ToChar(97+y);
+//         pair += secondLetter;
+//         wires[pair] = 0;
+//     }
+// }
--- obj/Debug/net8.0/drew.AssemblyInfo.cs
+++ obj/Debug/net8.0/drew.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("drew")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a051fe172adc3e1042225ba1ab68a45023c92bdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d470ec9d4e39a3a94747c79a560544363a6d36f")]
 [assembly: System.Reflection.AssemblyProductAttribute("drew")]
 [assembly: System.Reflection.AssemblyTitleAttribute("drew")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
